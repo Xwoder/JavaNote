c4114a7510bd592e79d26de3bd18031cfc66324a
Xwoder <Xwoder@qq.com>
2022-05-12 00:09:16 +0800

    Update StringCompare.java

--- src/StringCompare.java
+++ src/StringCompare.java
@@ -2,8 +2,18 @@ public class StringCompare {
     public static void main(String[] args) {
         final String s1 = new String("abc");
         final String s2 = new String("abc");
+        System.out.println(s1 == s2); /* false */
+        System.out.println(s1.equals(s2)); /* true */
 
-        System.out.println(s1 == s2);
-        System.out.println(s1.equals(s2));
+        final String s3 = "abc";
+        final String s4 = "abc";
+        System.out.println(s3 == s4);/* true */
+        System.out.println(s3.equals(s4));/* true */
+
+        final String s5 = new String("abc");
+        final String s6 = "abc";
+        System.out.println(s5 == s6); /* false */
+        System.out.println(s5.equals(s6)); /* true */
+
     }
 }
