64b9529469d87bb76128453a966211f20dd955ee
Xwoder <Xwoder@qq.com>
2022-05-11 23:40:12 +0800

    Create JavaComment.java

---
+++ src/JavaComment.java
@@ -0,0 +1,11 @@
+public class JavaComment {
+    /**
+     * main 函数
+     *
+     * @param args args 参数
+     */
+    public static void main(String[] args) {
+        // 打印字符串 Hello World
+        System.out.println("Hello World");
+    }
+}
